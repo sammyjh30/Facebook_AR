d43d1ec5620aee25b9418eed3b2933aa15a0949e
Meghan Westvig <mwestvig@student.wethinkcode.co.za>
2018-10-16 13:19:59 +0200

    Working equations and output values

--- functions.js
+++ functions.js
@@ -1,3 +1,7 @@
+var Patches = require('Patches');
+var Diagnostics = require('Diagnostics');
+var Scene = require('Scene');
+var arr = [];
 
 function randomNumber(min, max)
 {
@@ -6,29 +10,84 @@ function randomNumber(min, max)
 
 function equation()
 {
-  var oper = ['+', '-', '*', '/'];
-  var arr = [];
-  arr[0] = randomNumber(0, 20);
-  arr[1] = oper[randomNumber(0, 3)];
-  arr[2] = randomNumber(1, 20);
+  var oper = ['+', '-', 'x', '÷'];
+  arr[0] = randomNumber(0, 21);
+  arr[1] = oper[randomNumber(0, 4)];
+  arr[2] = randomNumber(1, 21);
   return (arr.join(" "));
 }
 
 function setX(equation)
 {
-  var out = eval(equation);
+  var l = Number(arr[0]);
+  var oper = arr[1];
+  var r = Number(arr[2]);
+  var out;
+  switch (oper) {
+      case ('+'):
+        out = (l + r).toFixed(0);
+        break;
+      case ('x'):
+        out = (l * r).toFixed(0);
+        break;
+      case ('÷'):
+        out = (l / r).toFixed(2);
+        break;
+      case ('-'):
+        out = (l - r).toFixed(0);
+        break;
+    }
   return out;
 }
 
 function setY(x)
 {
-  var out = randomNumber(x - 5, x + 5)
+  var out = randomNumber(parseInt(x) - 5, parseInt(x) + 5);
   return out;
 }
 
 var equation = equation();
 var x = setX(equation);
 var y = setY(x);
-console.log(equation);
-console.log(x);
-console.log(y);
+var options = [x, y];
+var choice = randomNumber(0, 2);
+
+Patches.setStringValue("equation", equation);
+Patches.setStringValue("x", x);
+Patches.setScalarValue("y", y);
+Scene.root.find("equation").text = equation;
+if (choice == 0)
+{
+  Scene.root.find("option1").text = "" + options[0] + "";
+  Scene.root.find("option2").text = "" + options[1] + "";
+}
+else
+{
+  Scene.root.find("option1").text = "" + options[1] + "";
+  Scene.root.find("option2").text = "" + options[0] + "";
+}
+Diagnostics.log(equation);
+Diagnostics.log(x);
+Diagnostics.log(y);
+
+
+// Available modules include (this is not a complete list):
+// var Scene = require('Scene');
+// var Textures = require('Textures');
+// var Materials = require('Materials');
+// var FaceTracking = require('FaceTracking');
+// var Animation = require('Animation');
+// var Reactive = require('Reactive');
+//
+// Example script
+//
+// Loading required modules
+// var Scene = require('Scene');
+// var FaceTracking = require('FaceTracking');
+//
+// Binding an object's property to a value provided by the face tracker
+// Scene.root.child('object0').transform.rotationY = FaceTracking.face(0).transform.rotationX;
+//
+// If you want to log objects, use the Diagnostics module.
+// var Diagnostics = require('Diagnostics');
+// Diagnostics.log(Scene.root);
