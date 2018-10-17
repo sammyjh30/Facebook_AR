202ac9b554a248292d9b85314a9c3eef0754d097
morpheus <morpheus@morpheuss-iMac.local>
2018-10-17 13:07:12 +0200

    Updating QuickMath to work

--- QuickMath_AR_v01/scripts/script.js
+++ QuickMath_AR_v01/scripts/script.js
@@ -18,6 +18,11 @@ var timerID;
 var timeStop;
 var score = 0;
 var addScore = false;
+var roundNum = 20;
+var check = Patches.getBooleanValue("check");
+var stopTime = Patches.getBooleanValue("stopTime");
+var newRound = Patches.getBooleanValue("newRound");
+var startTime = Patches.getBooleanValue("startTime");
 
 function randomNumber(min, max)
 {
@@ -73,89 +78,112 @@ if (out == x)
 return out;
 }
 
-function countdown() {
-	if (timeStop == true) {
-		// Diagnostics.log("Got remaining time");
-		// Diagnostics.log(timeLeft);
-		if (addScore == true) {
-			Patches.getBooleanValue("check").monitor().subscribe(function(b) {
-				if (b.newValue === true) {
-					score += timeLeft * 10;
-					Scene.root.find("score").text = score.toString();
-				}
-			});
-			
-		}
-		Scene.root.find("timer").text = "";
-		Time.clearTimeout(timerID);
-		timeLeft = 10;
-		addScore = false;
-	}
-	else if (timeLeft == 0) {
-		Time.clearTimeout(timerID);
-		Scene.root.find("timer").text = "TIME'S UP!";
-	}
-	else {
-		addScore = true;
-		Diagnostics.log(timeLeft);
-		timeStr = timeLeft.toString()
-		Scene.root.find("timer").text = timeStr;
-		timeLeft--;
-	}
-}
+// Diagnostics.log(timeLeft);
 
-Diagnostics.log(timeLeft);
-
-Patches.getBooleanValue("startTime").monitor().subscribe(function(e) {
-	Diagnostics.log(e.newValue);
+startTime.monitor().subscribe(function(e) {
+	// Diagnostics.log(e.newValue);
 	round();
-	});
+});
 
 function round() {
 	// Patches.setPulseValue("runPulse", Reactive.once());
 	// Patches.find("runPulse").trunOn = true;
 	// Patches.find("runSwitch").trunOff = false;
-	equation = ft_equation();
-	Diagnostics.log(equation);
-	x = setX(equation);
-	y = setY(x);
-	options = [x, y];
-	choice = randomNumber(0, 2);
-	right = options[0];
-	wrong = options[1];
-	Scene.root.find("equation").text = equation;
-	if (choice == 0){
-		
-		Scene.root.find("num1").text = right.toString();
-		Scene.root.find("num2").text = wrong.toString();
-		Patches.setScalarValue("num1", parseInt(right, 10));
-		Patches.setScalarValue("num2", parseInt(wrong, 10));
-	}
-	else{
-		Scene.root.find("num1").text = wrong.toString();
-		Scene.root.find("num2").text = right.toString();
-		Patches.setScalarValue("num1", parseInt(wrong, 10));
-		Patches.setScalarValue("num2", parseInt(right, 10));
-	}
+	Diagnostics.log(roundNum);
+	var gameOver = false;
+	roundNum--;
+	if (roundNum > 0){
+		equation = ft_equation();
+		// Diagnostics.log(equation);
+		x = setX(equation);
+		y = setY(x);
+		options = [x, y];
+		choice = randomNumber(0, 2);
+		right = options[0];
+		wrong = options[1];
+		Scene.root.find("equation").text = equation;
+		if (choice == 0){
+			
+			Scene.root.find("num1").text = right.toString();
+			Scene.root.find("num2").text = wrong.toString();
+			Patches.setScalarValue("num1", parseFloat(right, 10));
+			Patches.setScalarValue("num2", parseFloat(wrong, 10));
+		}
+		else{
+			Scene.root.find("num1").text = wrong.toString();
+			Scene.root.find("num2").text = right.toString();
+			Patches.setScalarValue("num1", parseFloat(wrong, 10));
+			Patches.setScalarValue("num2", parseFloat(right, 10));
+		}
 
-	Patches.setScalarValue("right", parseInt(right, 10));
-	Patches.setScalarValue("wrong", parseInt(wrong, 10));
-	Diagnostics.log(equation);
-	Diagnostics.log(x);
-	Diagnostics.log(y);
-	
-	if (score == 0)
-	{
+		Patches.setScalarValue("right", parseFloat(right, 10));
+		Patches.setScalarValue("wrong", parseFloat(wrong, 10));
+		// Diagnostics.log(equation);
+		// Diagnostics.log(x);
+		// Diagnostics.log(y);
+		
+
 		Scene.root.find("score").text = score.toString();
-		Patches.getBooleanValue("stopTime").monitor().subscribe(function(a) {
+		stopTime.monitor().subscribe(function(a) {
 			if (a.newValue === true)
 				timeStop = true;
 			else
 				timeStop = false;
-			});
+		});
 		timerID = Time.setInterval(countdown, 1000);
 	}
+	else
+		gameOver = true;
+	Patches.setBooleanValue("gameOver", gameOver);
 	// Patches.find("runSwitch").trunOn = false;
 	// Patches.find("runSwitch").trunOff = true;
 
+}
+
+function countdown() {
+	if (timeStop == true) {
+		// Diagnostics.log("Got remaining time");
+		// Diagnostics.log(timeLeft);
+		if (addScore == true) {
+			check.monitor().subscribe(function(b) {
+				if (b.newValue == true) {
+					score += timeLeft * 10;
+					Scene.root.find("score").text = score.toString();
+				}
+				else{
+					score += timeLeft * 10;
+					Scene.root.find("score").text = score.toString();
+				}
+
+			});
+			// if (Patches.getBooleanValue("check").monitor() == true){
+			// 	score += timeLeft * 10;
+			// 		Scene.root.find("score").text = score.toString();
+			// }
+		}
+		newRound.monitor().subscribe(function(b) {
+			if (b.newValue == true) {
+				resetRound();
+			}
+		});
+		
+	}
+	else if (timeLeft == 0) {
+		Scene.root.find("timer").text = "TIME'S UP!";
+		resetRound();
+	}
+	else {
+		addScore = true;
+		// Diagnostics.log(timeLeft);
+		Scene.root.find("timer").text = timeLeft.toString();
+		timeLeft--;
+	}
+}
+
+function resetRound(){
+	Time.clearTimeout(timerID);
+	timeLeft = 10;
+	Scene.root.find("timer").text = timeLeft.toString();
+	addScore = false;
+	round();
 }
