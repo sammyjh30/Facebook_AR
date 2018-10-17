a9bf1712499943647802da402d647ce72090a146
Xeno RHODA <xrhoda@c1r3s4.wethinkcode.co.za>
2018-10-17 08:09:08 +0200

    submit

--- QuickMath_AR_v01/scripts/script.js
+++ QuickMath_AR_v01/scripts/script.js
@@ -4,6 +4,21 @@ var Diagnostics = require('Diagnostics');
 var Patches = require('Patches');
 var arr = [];
 
+var equation;
+var x;
+var y;
+var options;
+var choice;
+var right;
+var wrong;
+
+var timeLeft = 10;
+var timeStr;
+var timerID;
+var timeStop;
+var score = 0;
+var addScore = false;
+
 function randomNumber(min, max)
 {
 	return Math.floor((Math.random() * (max - min) + min));
@@ -14,7 +29,7 @@ function randomDecimal(min, max)
 	return (Math.random() * (max - min) + min);
 }
 
-function equation()
+function ft_equation()
 {
 var oper = ['+', '-', 'x', '÷'];
 arr[0] = randomNumber(0, 21);
@@ -53,76 +68,35 @@ if (arr[1] == '÷')
 	out = randomDecimal(parseInt(x) - 5, parseInt(x) + 5).toFixed(2);
 else
 	out = randomNumber(parseInt(x) - 5, parseInt(x) + 5).toFixed(0);
-//if (out == x)
-	//out++;
+if (out == x)
+	out++;
 return out;
 }
 
-var equation = equation();
-var x = setX(equation);
-var y = setY(x);
-var options = [x, y];
-var choice = randomNumber(0, 2);
-var right;
-var wrong;
-
-// Patches.setStringValue("equation", equation);
-// Patches.setStringValue("x", x);
-// Patches.setStringValue("y", y);
-right = options[0];
-wrong = options[1];
-Scene.root.find("equation").text = equation;
-if (choice == 0){
-	
-	Scene.root.find("num1").text = right.toString();
-	Scene.root.find("num2").text = wrong.toString();
-	Patches.setScalarValue("num1", parseInt(right, 10));
-	Patches.setScalarValue("num2", parseInt(wrong, 10));
-}
-else{
-	Scene.root.find("num1").text = wrong.toString();
-	Scene.root.find("num2").text = right.toString();
-	Patches.setScalarValue("num1", parseInt(wrong, 10));
-	Patches.setScalarValue("num2", parseInt(right, 10));
-}
-
-Patches.setScalarValue("right", parseInt(right, 10));
-Patches.setScalarValue("wrong", parseInt(wrong, 10));
-Diagnostics.log(equation);
-Diagnostics.log(x);
-Diagnostics.log(y);
-
-var timeLeft = 10;
-var timeStr;
-var timerID;
-var timeStop;
-var score = 0;
-
-var Mouth = false;
-if (score == 0)
-	Scene.root.find("score").text = score.toString();
-	Mouth = Patches.getBooleanValue('check').monitor().subscribe(function(e) {
-		Diagnostics.log(e.newValue);
-		timeStop = e.newValue;
-		if (e.newValue) {
-			timerID = Time.setInterval(countdown, 1000);
-		}
-});
 function countdown() {
-	if (timeStop === false) {
+	if (timeStop == true) {
+		// Diagnostics.log("Got remaining time");
+		// Diagnostics.log(timeLeft);
+		if (addScore == true) {
+			Patches.getBooleanValue("check").monitor().subscribe(function(b) {
+				if (b.newValue === true) {
+					score += timeLeft * 10;
+					Scene.root.find("score").text = score.toString();
+				}
+			});
+			
+		}
 		Scene.root.find("timer").text = "";
-		Diagnostics.log("Got remaining time");
-		Diagnostics.log(timeLeft);
-		score += timeLeft * 10;
-		Scene.root.find("score").text = score.toString();
 		Time.clearTimeout(timerID);
 		timeLeft = 10;
+		addScore = false;
 	}
 	else if (timeLeft == 0) {
 		Time.clearTimeout(timerID);
 		Scene.root.find("timer").text = "TIME'S UP!";
 	}
 	else {
+		addScore = true;
 		Diagnostics.log(timeLeft);
 		timeStr = timeLeft.toString()
 		Scene.root.find("timer").text = timeStr;
@@ -130,24 +104,58 @@ function countdown() {
 	}
 }
 
-// Available modules include (this is not a complete list):
-// var Scene = require('Scene');
-// var Textures = require('Textures');
-// var Materials = require('Materials');
-// var FaceTracking = require('FaceTracking');
-// var Animation = require('Animation');
-// var Reactive = require('Reactive');
-//
-// Example script
-//
-// Loading required modules
-// var Scene = require('Scene');
-// var FaceTracking = require('FaceTracking');
-//
-// Binding an object's property to a value provided by the face tracker
-// Scene.root.child('object0').transform.rotationY = FaceTracking.face(0).transform.rotationX;
-//
-// If you want to log objects, use the Diagnostics module.
-// var Diagnostics = require('Diagnostics');
-// Diagnostics.log(Scene.root);
+Diagnostics.log(timeLeft);
 
+Patches.getBooleanValue("startTime").monitor().subscribe(function(e) {
+	Diagnostics.log(e.newValue);
+	round();
+	});
+
+function round() {
+	// Patches.setPulseValue("runPulse", Reactive.once());
+	// Patches.find("runPulse").trunOn = true;
+	// Patches.find("runSwitch").trunOff = false;
+	equation = ft_equation();
+	Diagnostics.log(equation);
+	x = setX(equation);
+	y = setY(x);
+	options = [x, y];
+	choice = randomNumber(0, 2);
+	right = options[0];
+	wrong = options[1];
+	Scene.root.find("equation").text = equation;
+	if (choice == 0){
+		
+		Scene.root.find("num1").text = right.toString();
+		Scene.root.find("num2").text = wrong.toString();
+		Patches.setScalarValue("num1", parseInt(right, 10));
+		Patches.setScalarValue("num2", parseInt(wrong, 10));
+	}
+	else{
+		Scene.root.find("num1").text = wrong.toString();
+		Scene.root.find("num2").text = right.toString();
+		Patches.setScalarValue("num1", parseInt(wrong, 10));
+		Patches.setScalarValue("num2", parseInt(right, 10));
+	}
+
+	Patches.setScalarValue("right", parseInt(right, 10));
+	Patches.setScalarValue("wrong", parseInt(wrong, 10));
+	Diagnostics.log(equation);
+	Diagnostics.log(x);
+	Diagnostics.log(y);
+	
+	if (score == 0)
+	{
+		Scene.root.find("score").text = score.toString();
+		Patches.getBooleanValue("stopTime").monitor().subscribe(function(a) {
+			if (a.newValue === true)
+				timeStop = true;
+			else
+				timeStop = false;
+			});
+		timerID = Time.setInterval(countdown, 1000);
+	}
+	// Patches.find("runSwitch").trunOn = false;
+	// Patches.find("runSwitch").trunOff = true;
+
+}
